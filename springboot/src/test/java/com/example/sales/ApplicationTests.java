bb523594b74bddfde039c0e95842fd0b2bdeeed5
eitnadim <nadim@ethicalintelligent.com>
2026-06-29 09:43:16 +0530

    Add springboot/src/test/java/com/example/sales/ApplicationTests.java

---
+++ springboot/src/test/java/com/example/sales/ApplicationTests.java
@@ -0,0 +1,6 @@
+package com.example.sales;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest class ApplicationTests {
+    @Test void contextLoads() {}
+}
